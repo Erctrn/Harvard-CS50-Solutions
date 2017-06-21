c703283e249412d87a8b4f90ff2b9b868e5ce190
erctrn <erictrann@hotmail.com>
2017-06-21 10:25:15 -0700

    vigenere in progress

---
+++ pset2/vigenere.c
@@ -0,0 +1,17 @@
+/*
+Eric Tran
+cs50x
+vigenere.c
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[]) 
+{
+
+return 0;
+}
